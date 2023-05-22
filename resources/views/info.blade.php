4041556dbed1c01aef533e4a313e80d104bcfabc
Dan848 <daniele.ceravolo15@gmail.com>
2023-05-22 02:01:49 +0200

    add nav with route

---
+++ resources/views/info.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel Primi Passi</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Fonts Awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- Bootstrap Framework -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    </head>
+    <body class="bg-body">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+            <div class="container">
+                <a class="navbar-brand" href="{{route($nav_bar["home"][1])}}">{{$app_title}}</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    @foreach ($nav_bar as $link)
+                        <a class="nav-link" href="{{ route($link[1])}}">{{$link[0]}}</a>
+                    @endforeach
+                </div>
+              </div>
+            </div>
+        </nav>
+        <div class="container">
+            <h1 class="text-center mt-4 mb-5">{{$page_title}}</h1>
+            <p>{{$paragraph}}</p>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,110 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $data = [
+        "app_title" => "Primi Passi",
+        "page_title" => "Primi Passi",
+        "paragraph" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio assumenda, dolor labore quidem totam consequuntur pariatur corporis exercitationem obcaecati similique doloremque, aspernatur officia placeat consectetur sint excepturi fugit necessitatibus reprehenderit laudantium atque! Quis quasi facere ipsum maiores dignissimos laboriosam magni nesciunt, laborum error? Iste voluptate consectetur ipsam officia veniam iusto!",
+        "nav_bar" => [
+            "home" => [
+                "Home",
+                "home"
+            ],
+            "contatti" => [
+                "Contatti",
+                "contacts"
+            ],
+            "third_page" => [
+                "Terza Pagina",
+                "third"
+            ],
+            "info" => [
+                "Info",
+                "info"
+            ],
+        ],
+    ];
+    return view('home', $data);
+})->name("home");
+
+Route::get('/contacts', function () {
+    $data = [
+        "app_title" => "Primi Passi",
+        "page_title" => "Contatti",
+        "paragraph" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio assumenda, dolor labore quidem totam consequuntur pariatur corporis exercitationem obcaecati similique doloremque, aspernatur officia placeat consectetur sint excepturi fugit necessitatibus reprehenderit laudantium atque! Quis quasi facere ipsum maiores dignissimos laboriosam magni nesciunt, laborum error? Iste voluptate consectetur ipsam officia veniam iusto!",
+        "nav_bar" => [
+            "home" => [
+                "Home",
+                "home"
+            ],
+            "contatti" => [
+                "Contatti",
+                "contacts"
+            ],
+            "third_page" => [
+                "Terza Pagina",
+                "third"
+            ],
+            "info" => [
+                "Info",
+                "info"
+            ],
+        ],
+    ];
+    return view('contacts', $data);
+})->name("contacts");
+
+
+Route::get('/third', function () {
+    $data = [
+        "app_title" => "Primi Passi",
+        "page_title" => "Terza Pagina",
+        "paragraph" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio assumenda, dolor labore quidem totam consequuntur pariatur corporis exercitationem obcaecati similique doloremque, aspernatur officia placeat consectetur sint excepturi fugit necessitatibus reprehenderit laudantium atque! Quis quasi facere ipsum maiores dignissimos laboriosam magni nesciunt, laborum error? Iste voluptate consectetur ipsam officia veniam iusto!",
+        "nav_bar" => [
+            "home" => [
+                "Home",
+                "home"
+            ],
+            "contatti" => [
+                "Contatti",
+                "contacts"
+            ],
+            "third_page" => [
+                "Terza Pagina",
+                "third"
+            ],
+            "info" => [
+                "Info",
+                "info"
+            ],
+        ],
+    ];
+    return view('third', $data);
+})->name("third");
+
+Route::get('/info', function () {
+    $data = [
+        "app_title" => "Primi Passi",
+        "page_title" => "Info",
+        "paragraph" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio assumenda, dolor labore quidem totam consequuntur pariatur corporis exercitationem obcaecati similique doloremque, aspernatur officia placeat consectetur sint excepturi fugit necessitatibus reprehenderit laudantium atque! Quis quasi facere ipsum maiores dignissimos laboriosam magni nesciunt, laborum error? Iste voluptate consectetur ipsam officia veniam iusto!",
+        "nav_bar" => [
+            "home" => [
+                "Home",
+                "home"
+            ],
+            "contatti" => [
+                "Contatti",
+                "contacts"
+            ],
+            "third_page" => [
+                "Terza Pagina",
+                "third"
+            ],
+            "info" => [
+                "Info",
+                "info"
+            ],
+        ],
+    ];
+    return view('info', $data);
+})->name("info");
